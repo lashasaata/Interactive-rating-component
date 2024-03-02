b5474a37a996db7cf91a7621efcd7665eaf7fcb2
lashasaata <lashasaata@gmail.com>
2024-03-02 12:06:20 -0800

    add some changes in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import { useState } from "react";
+import styled from "styled-components";
 import "./App.css";
+import Ratting from "./components/ratting/ratting";
+import Result from "./components/result/result";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  const [number, setNumber] = useState();
+  const [result, setResult] = useState(true);
+  return (
+    <Pragment>
+      {result ? (
+        <Ratting
+          number={number}
+          setNumber={setNumber}
+          result={result}
+          setResult={setResult}
+        />
+      ) : (
+        <Result number={number} />
+      )}
+    </Pragment>
+  );
 }
 
 export default App;
+
+const Pragment = styled.div`
+  background-color: #131518;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
